b6ef050619a20d9213988806e64450b1290ecd18
Adi Rai <pindi251210@gmail.com>
2018-03-05 04:51:12 +0000

    added extra pages

---
+++ Public/main.js
@@ -0,0 +1,61 @@
+var menuContent = document.querySelector(".menu-content");
+var menuMini = document.querySelector(".menu-mini");
+var albumCover = document.querySelectorAll(".covers");
+var albumInfo = document.querySelectorAll(".album-info");
+var submit = document.querySelector("#submit");
+
+
+
+menuMini.addEventListener("click", function() {
+  menuContent.style.display = "block";
+});
+
+menuContent.addEventListener("click", function() {
+  menuContent.style.display = "none";
+});
+
+
+albumCoverDisplay();
+
+
+function albumCoverDisplay() {
+
+  var i = 0;
+  for (i = 0; i < albumCover.length; i++) {
+    albumCover[i].addEventListener("click", function() {
+
+      var j = 0;
+      for (var j = 0; j < albumCover.length; j++) {
+        albumCover[j].style.display = "inline-block";
+      }
+      this.style.display = "none";
+      if (albumCover[0].style.display = "none") {
+        albumInfo[0].style.display = "block";
+      }
+      if (albumCover[1].style.display = "none") {
+        albumInfo[1].style.display = "block";
+      }
+      if (albumCover[2].style.display = "none") {
+        albumInfo[2].style.display = "block";
+      }
+      if (albumCover[3].style.display = "none") {
+        albumInfo[3].style.display = "block";
+      }
+      if (albumCover[4].style.display = "none") {
+        albumInfo[4].style.display = "block";
+      }
+    });
+  }
+}
+
+submit.addEventListener("click", function() {
+  var name = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  if ((name || email) !== "") {
+    alert("Name and Email must be filled out");
+  } else {
+
+    alert("Your message has been submitted");
+  }
+
+});
